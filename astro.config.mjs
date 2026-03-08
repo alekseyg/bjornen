e4a1c721be9e4d3a4ed713f67d33e8b46f1fd124
Aleksey Gladysh <aleksey.gladysh@gmail.com>
2026-03-07 23:25:29 -0500

    Deployment tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.bjornen.me',
+	site: 'https://bjornen.me',
 	integrations: [mdx(), sitemap()],
 });
